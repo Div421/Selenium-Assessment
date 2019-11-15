59d81134a397c5eff66e2428f7a770f308f2a4e6
Div421 <44995655+Div421@users.noreply.github.com>
2019-11-15 16:39:16 +0530

    Add files via upload

---
+++ DataDrivenFramework/src/com/ddf/DataDrivenFramework.java
@@ -0,0 +1,131 @@
+package com.ddf;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DataDrivenFramework {
+	
+	public static String vSearch; 
+	public static int xlRows;
+	public static int xlCols;
+	public static String xData[][];
+	
+	public static void main(String[] args) throws Exception {
+		DataDrivenFramework Ddf = new DataDrivenFramework();
+		Ddf.getResults();
+	}
+	
+	//Searches the data present in excel sheet in yahoo and saves in a new excel file
+	public void getResults() throws Exception {
+		xlRead("C:\\Users\\ddivya24\\Documents\\Selenium-Testing\\YahooDDF.xls");
+		
+		for(int i = 1;i< xlRows;i++) {
+			
+			//Search function will be performed when execution column in excel has value Y
+			if(xData[i][1].equalsIgnoreCase("Y")) {
+					vSearch = xData[i][0];
+					System.setProperty("webdriver.chrome.driver","C:\\Selenium Jar Files\\chromedriver_win32\\chromedriver.exe");
+					WebDriver driver = new ChromeDriver();
+					driver.manage().window().maximize();
+					driver.get("https://www.yahoo.com/");
+					driver.findElement(By.id("uh-search-box")).sendKeys(vSearch);
+					Thread.sleep(2000);
+					driver.findElement(By.id("uh-search-button")).click();
+					Thread.sleep(2000);
+					xData[i][2]=driver.getTitle();
+					driver.close();
+			}
+			xlwrite("C:\\Users\\ddivya24\\Documents\\Selenium-Testing\\YahooDDFResults1.xls",xData);
+		}
+	}
+	
+	//Excel : read method
+	public static void xlRead(String sPath) throws Exception
+	{
+		File myFile = new File(sPath);
+		FileInputStream myStream=new FileInputStream(myFile);
+		HSSFWorkbook myworkbook=new HSSFWorkbook(myStream);
+		HSSFSheet mySheet=myworkbook.getSheetAt(0);
+		xlRows=mySheet.getLastRowNum()+1;
+		xlCols=mySheet.getRow(0).getLastCellNum();
+		xData=new String[xlRows][xlCols];
+		for(int i=0;i<xlRows;i++)
+		{
+			HSSFRow row=mySheet.getRow(i);
+			for(short j=0;j<xlCols;j++)
+			{
+				HSSFCell cell=row.getCell(j);
+				String value=cellToString(cell);
+				xData[i][j]=value;
+				System.out.print("-"+xData[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	    //Excel :Cell to String method
+		public static String cellToString(HSSFCell cell)
+		{
+			int type=cell.getCellType();
+			Object result;
+			switch(type)
+			{
+			case HSSFCell.CELL_TYPE_NUMERIC:
+			result=cell.getNumericCellValue();
+			break;
+			case HSSFCell.CELL_TYPE_STRING:
+			result=cell.getStringCellValue();
+			break;
+			case HSSFCell.CELL_TYPE_FORMULA:
+			throw new RuntimeException("We cannot evaluate formula");
+			case HSSFCell.CELL_TYPE_BLANK:
+			result="-";
+			case HSSFCell.CELL_TYPE_BOOLEAN:
+			result=cell.getBooleanCellValue();
+			case HSSFCell.CELL_TYPE_ERROR:
+			result="This cell has some error";
+			default:
+			throw new RuntimeException("We do not support this cell type");
+			}
+			return result.toString();
+			
+		}
+		
+		//Excel : write method
+		public static void xlwrite(String xlpath1,String[][] xData) throws Exception
+		{
+			System.out.println("Inside XL Write");
+			File myFile1=new File(xlpath1);
+			FileOutputStream fout=new FileOutputStream(myFile1);
+			HSSFWorkbook wb=new HSSFWorkbook();
+			HSSFSheet mySheet1=wb.createSheet("TestResults");
+			for(int i=0;i<xlRows;i++)
+			{
+				HSSFRow row1=mySheet1.createRow(i);
+				for(short j=0;j<xlCols;j++)
+				{
+					HSSFCell cell1=row1.createCell(j);
+					cell1.setCellType(HSSFCell.CELL_TYPE_STRING);
+					cell1.setCellValue(xData[i][j]);
+				}
+			}
+			wb.write(fout);
+			fout.flush();
+			fout.close();
+		}
+
+	
+	
+
+
+}
